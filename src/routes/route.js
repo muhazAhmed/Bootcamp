3ea132d165c673ef60bc21241ba59a8f5bcc3f21
Muhaz <muhazvla313@gmail.com>
2022-08-08 12:19:24 +0530

    removed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const abc = require('../introduction/intro')
-const bcd = require('../logger/logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
-    bcd.printInfo()
     res.send('My second ever api!')
 });
 
