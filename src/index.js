cc8d129c616a075892bc15313ad080a3fd73a3aa
Muhaz <muhazvla313@gmail.com>
2022-08-09 12:01:52 +0530

    Added all pending assignments

--- src/index.js
+++ src/index.js
@@ -7,4 +7,63 @@ app.get('/movies',(req,res)=>{
 })
 
 
+app.get('/movies/:indexNumber',(req,res)=>{
+    let movies = ['Rang De Basanti','The shining','Batman begins']
+    console.log(req.params.indexNumber);
+    let movieIndex = req.params.indexNumber
+
+    let requiredMovie =  movies[movieIndex]
+    res.send(requiredMovie)
+})
+
+app.get('/movies/:indexNumber',(req,res)=>{
+    let movies = ['Rang De Basanti','The shining','Batman begins']
+    console.log(req.params.indexNumber);
+    let movieIndex = req.params.indexNumber
+    if (movieIndex<0 || movieIndex>=movies.length){
+        return res.send('The index value is wrong')
+    }
+    let requiredMovie = movies[movieIndex]
+    res.send(requiredMovie)
+})
+
+app.get('/films', function(req,res){
+    let films = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+    res.send(films)   
+})
+
+app.get('/films/:filmId', function(req,res){
+    let films = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+    let filmId = req.params.filmId
+    for(i = 0; i< films.length; i++){
+        let movie = films[i]
+        if(movie.id == filmId){
+            return res.send(movie)
+        }
+    }res.send('Wrong id')
+})       
 app.listen(3000)
