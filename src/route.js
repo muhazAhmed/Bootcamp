471e464d08878c9404afd2eee4128a6a4f90610e
Muhaz <muhazvla313@gmail.com>
2022-08-10 13:28:45 +0530

    post-api

---
+++ src/route.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const express = require("express")
+const router = express();
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+router.post('/players', function (req, res) {
+    let ele = req.body.players
+    players.push(ele)
+    res.send(  { data: players , status: true }  )
+})
+router.listen(3000)
