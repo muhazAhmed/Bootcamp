5fa331e6ecb48b87704b957faffccbbdfc93d860
Muhaz <muhazvla313@gmail.com>
2022-08-17 00:08:46 +0530

    mongo/session1

---
+++ src/models/bookModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName: String, 
+    authorName: String, 
+    tags: [String],
+    
+    isPublished: Boolean,
+    prices: {
+        indianPrice: String,
+        europePrice: String,
+    },
+    sales: {type: Number, default: 10},
+    year: {type: Number, default: 1997}
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Book', bookSchema)
