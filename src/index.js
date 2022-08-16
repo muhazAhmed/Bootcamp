cce4cb937987204e505b47df26e2c737b2e7dae4
muhazAhmed <109068603+muhazAhmed@users.noreply.github.com>
2022-08-16 23:11:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://muhaz:6VE8Lk82R6vAuBok@cluster0.syf7fzi.mongodb.net/muhaz-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
